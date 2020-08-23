d47718248b14055f63900fe448119e0829d93cb3
Forrest Tracy <forrest.j.tracy@gmail.com>
2020-08-23 18:44:59 -0500

    migrate ViewPager to ViewPager2

--- app/src/main/java/com/tracytech/goattipcalculator/MainActivity.kt
+++ app/src/main/java/com/tracytech/goattipcalculator/MainActivity.kt
@@ -4,22 +4,30 @@ import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import com.tracytech.goattipcalculator.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAB_TITLES = arrayOf(
+        R.string.restaurant_tab_text,
+        R.string.hair_tab_text,
+        R.string.ride_tab_text
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager, lifecycle)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = (resources.getString(TAB_TITLES[position]))
+        }.attach()
+
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener { view ->
--- app/src/main/java/com/tracytech/goattipcalculator/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/tracytech/goattipcalculator/ui/main/SectionsPagerAdapter.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.tracytech.goattipcalculator.R
 
 private val TAB_TITLES = arrayOf(
@@ -16,19 +18,16 @@ private val TAB_TITLES = arrayOf(
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fm, lifecycle) {
 
-    override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
-    }
-
-    override fun getCount(): Int {
+    override fun getItemCount(): Int {
         return TAB_TITLES.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        // position1 = restaurant
+        // position1 = hair
+        // position1 = ride
+        return PlaceholderFragment.newInstance(position + 1)
     }
 }
